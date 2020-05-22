91bd29c580846d3e695ee57629729264753a78fa
Krzysztof Lecki <klecki@nvidia.com>
2020-05-22 20:33:06 +0200

    Initial convolution work [WIP]
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

---
+++ dali/kernels/imgproc/convolution/separable_convolution.h
@@ -0,0 +1,284 @@
+// Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DALI_KERNELS_IMGPROC_CONVOLUTION_SEPARABLE_CONVOLUTION_H_
+#define DALI_KERNELS_IMGPROC_CONVOLUTION_SEPARABLE_CONVOLUTION_H_
+
+#include "dali/core/tensor_view.h"
+#include "dali/kernels/common/utils.h"
+#include "dali/kernels/kernel.h"
+
+namespace dali {
+namespace kernels {
+
+// should it be channel aware?
+// let's assume that yes. and that channels are always dense
+template <typename T>
+class CyclicPixelWrapper {
+ public:
+  CyclicPixelWrapper(T* ptr, int length, int num_channels)
+      : data(ptr), start(0), end(0), elements(0), length(length), num_channels(num_channels) {}
+
+  void PopPixel() {
+    assert(elements > 0);
+    elements--;
+    start++;
+    WrapPosition(start);
+  }
+
+  void PushPixel(const T* input) {
+    assert(elements < length);
+    for (int c = 0; c < num_channels; c++) {
+      data[end * num_channels + c] = *input;  // todo offset=end*num_channels; offset++
+      input++;
+    }
+    elements++;
+    end++;
+    WrapPosition(end);
+  }
+
+  void PushPixel(span<const T> input) {
+    assert(elements < length);
+    for (int c = 0; c < num_channels; c++) {
+      data[end * num_channels + c] = input[c];  // todo offset=end*num_channels; offset++
+    }
+    elements++;
+    end++;
+    WrapPosition(end);
+  }
+
+  T* GetPixelOffset(int idx) {
+    assert(idx < elements);
+    if (start + idx < length) {
+      return data + (start + idx) * num_channels;
+    } else {
+      return data + (start + idx - length) * num_channels;
+    }
+  }
+
+  // todo: move the if above loop
+  template <typename W>
+  void CalculateDot(W* accum, const W* window) {
+    assert(elements == length);
+    for (int c = 0; c < num_channels; c++) {
+      accum[c] = 0;
+    }
+    for (int idx = 0; idx < length; idx++) {
+      const auto* pixel = GetPixelOffset(idx);
+      for (int c = 0; c < num_channels; c++) {
+        accum[c] += window[idx] * pixel[c];
+      }
+    }
+  }
+
+  // size in pixels
+  int Size() {
+    return elements;
+  }
+
+  bool Empty() {
+    return elements == 0;
+  }
+
+  int NumChannels() {
+    return num_channels;
+  }
+
+ private:
+  void WrapPosition(int& pos) {
+    if (pos == length) {
+      pos = 0;
+    }
+  }
+
+  T* data = nullptr;
+  int start = 0;
+  int end = 0;  ///< next empty element
+  int elements = 0;
+  int length = 0;
+  int num_channels = 0;
+};
+
+// template <int border_type, typename T>
+// const T* fill_input_window(CyclicPixelWrapper<T>& in_window, const T* in, int64_t stride,
+//                            span<const T> fill_value, int num_channels, int r, int d) {
+//   for (int i = 0; i < r; i++) {
+//     in_window.PushPixel(fill_value);
+//   }
+//   // we insert center element + radius hence `<=`
+//   for (int i = 0; i <= r; i++) {  // TODO: DAMN BORDER CONDITIONS
+//     in_window.PushPixel(in);
+//     in += stride;
+//   }
+//   return in;
+// }
+
+template <typename T>
+void load_pixel_with_border(CyclicPixelWrapper<T>& cpw, const T* in_ptr, int in_idx, int stride,
+                            int axis_size, span<const T> fill_value) {
+  if (in_idx < 0) {
+    cpw.PushPixel(fill_value);
+  } else if (in_idx < axis_size) {
+    cpw.PushPixel(in_ptr + in_idx * stride);
+  } else {
+    cpw.PushPixel(fill_value);
+  }
+}
+
+template <typename T>
+void load_pixel_no_border(CyclicPixelWrapper<T>& cpw, const T* in_ptr, int in_idx, int stride) {
+  cpw.PushPixel(in_ptr + in_idx * stride);
+}
+
+
+template <typename T>
+struct inspect;
+
+// we're in channel dim
+template <int dim = 0, typename Out, typename In, typename W, int ndim>
+std::enable_if_t<dim == ndim - 1> traverse_axes(Out* out, const In* in, const W* window, int axis,
+                                                const TensorShape<ndim>& shape,
+                                                const TensorShape<ndim>& strides, int d,
+                                                int64_t offset, span<const In> border_fill, In* input_window_buffer,
+                                                span<W> pixel_tmp) {
+  auto pixel_stride = strides[axis];
+  auto axis_size = shape[axis];
+  auto num_channels = shape[ndim - 1];  // channel-last is assumed
+  int r = (d - 1) / 2;                  // radius = (diameter - 1) / 2
+  // offset <- start of current axis
+  auto* out_ptr = out + offset;
+  auto* in_ptr = in + offset;
+  // prolog: fill input window
+
+  CyclicPixelWrapper<In> input_window(input_window_buffer, d, num_channels);
+
+  constexpr int Border = 0;  // FILL
+
+  int in_idx = -r, out_idx = 0;
+  for (in_idx = -r; in_idx < 0; in_idx++) {
+    load_pixel_with_border(input_window, in_ptr, in_idx, pixel_stride, axis_size, border_fill);
+  }
+  // the window fits in axis
+  if (r < axis_size) {
+    // we load the rest of window
+    for (; in_idx <= r; in_idx++) {
+      load_pixel_no_border(input_window, in_ptr, in_idx, pixel_stride);
+    }
+    for (; out_idx < axis_size - r; out_idx++, in_idx++) {
+      // TODO we assume channel-last, still this can be rewritten as two linear loops if compiler
+      // doesn't realize
+      // Thanks to the input_window we have a copy of all values and can store outputs directly
+      input_window.CalculateDot(pixel_tmp.data(), window);
+      for (int c = 0; c < num_channels; c++) {
+        out_ptr[out_idx + c] = pixel_tmp[c];  // todo scale & clamp
+      }
+      input_window.PopPixel();
+      load_pixel_no_border(input_window, in_ptr, in_idx, pixel_stride);
+    }
+  }
+  // the widow didn't fit
+  else {
+    // we need to load the rest of the window, just handle all with border condition for simplicity
+    for (; in_idx <= r; in_idx++) {
+      load_pixel_with_border(input_window, in_ptr, in_idx, pixel_stride, axis_size, border_fill);
+    }
+  }
+  // we need write out the rest of the outputs, the input window is full of data
+  for (; out_idx < axis_size; out_idx++, in_idx++) {
+    input_window.CalculateDot(pixel_tmp.data(), window);
+    for (int c = 0; c < num_channels; c++) {
+      out_ptr[out_idx + c] = pixel_tmp[c];  // todo scale & clamp
+    }
+    input_window.PopPixel();
+    load_pixel_with_border(input_window, in_ptr, in_idx, pixel_stride, axis_size, border_fill);
+  }
+}
+
+/// todo:rename
+template <int dim = 0, typename Out, typename In, typename W, int ndim>
+std::enable_if_t<(dim < ndim - 1)> traverse_axes(Out* out, const In* in, const W* window, int axis,
+                                                 const TensorShape<ndim>& shape,
+                                                 const TensorShape<ndim>& strides, int d,
+                                                 int64_t offset, span<const In> border_fill, In* input_window_buffer,
+                                                 span<W> pixel_tmp) {
+  if (dim == axis) {
+    traverse_axes<dim + 1>(out, in, window, axis, shape, strides, d, offset, border_fill, input_window_buffer,
+                           pixel_tmp);
+  } else if (dim != axis) {
+    for (int64_t i = 0; i < shape[dim]; i++) {
+      traverse_axes<dim + 1>(out, in, window, axis, shape, strides, d, offset, border_fill, input_window_buffer,
+                             pixel_tmp);
+      offset += strides[dim];
+    }
+  }
+}
+
+template <typename Out, typename In, typename W, int ndim>
+struct SeparableConvolution {
+  KernelRequirements Setup(KernelContext& ctx, const InTensorCPU<In, ndim>& in,
+                           const TensorView<StorageCPU, const W, 1>& window, int axis) {
+    KernelRequirements req;
+    ScratchpadEstimator se;
+    int channel_dim = ndim - 1;
+    DALI_ENFORCE(channel_dim == ndim - 1,
+                 "Only channel-last inputs are supposed, even for 1-channel data");
+    se.add<In>(AllocType::Host, GetInputWindowBufSize(in, window, channel_dim));
+    se.add<In>(AllocType::Host, GetPixelSize(in, channel_dim));  // fill value
+    se.add<W>(AllocType::Host, GetPixelSize(in, channel_dim));  // tmp result
+    req.scratch_sizes = se.sizes;
+    req.output_shapes.push_back(uniform_list_shape<ndim>(1, in.shape));
+    return req;
+  }
+
+  void Run(KernelContext& ctx, const TensorView<StorageCPU, Out, ndim> out,
+           const TensorView<StorageCPU, const In, ndim>& in,
+           const TensorView<StorageCPU, const W, 1>& window, int axis, int channel_dim = -1) {
+    DALI_ENFORCE(0 <= axis && axis < ndim - 1, "Cannot apply filter in channel dimension");  // todo
+    DALI_ENFORCE(channel_dim == -1 || channel_dim == ndim - 1,
+                 "Only no-channel or channel-last are supported.");
+    DALI_ENFORCE(channel_dim == ndim - 1,
+                 "Only channel-last inputs are supposed, even for 1-channel data");
+    int num_channels = GetPixelSize(in, channel_dim);
+    int input_window_buf_size = GetInputWindowBufSize(in, window, channel_dim);
+    auto* input_window_buffer = ctx.scratchpad->Allocate<In>(AllocType::Host, input_window_buf_size);
+    auto* border_fill_buf = ctx.scratchpad->Allocate<In>(AllocType::Host, num_channels);
+    auto* pixel_tmp_buf = ctx.scratchpad->Allocate<W>(AllocType::Host, num_channels);
+    auto strides = GetStrides(in.shape);
+    auto diameter = window.num_elements();
+
+    auto border_fill = make_span(border_fill_buf, num_channels);
+    // WIP
+    border_fill[0] = 0;
+    border_fill[1] = 0;
+    border_fill[2] = 0;
+    auto pixel_tmp = make_span(pixel_tmp_buf, num_channels);
+    // inspect<decltype(in.data)> x;
+
+    traverse_axes<0, Out, In, W, ndim>(out.data, in.data, window.data, axis, in.shape, strides, diameter, 0, border_fill, input_window_buffer, pixel_tmp);
+  }
+
+ private:
+  int GetInputWindowBufSize(const TensorView<StorageCPU, const In, ndim>& in,
+                            const TensorView<StorageCPU, const W, 1>& window, int channel_dim) {
+    return GetPixelSize(in, channel_dim) * window.num_elements();
+  }
+  int GetPixelSize(const TensorView<StorageCPU, const In, ndim>& in, int channel_dim) {
+    return channel_dim == -1 ? 1 : in.shape[channel_dim];
+  }
+};
+
+}  // namespace kernels
+}  // namespace dali
+
+#endif  // DALI_KERNELS_IMGPROC_CONVOLUTION_SEPARABLE_CONVOLUTION_H_
---
+++ dali/kernels/imgproc/convolution/separable_convolution_cpu_test.cc
@@ -0,0 +1,195 @@
+// Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+#include <cmath>
+#include <complex>
+#include <tuple>
+#include <vector>
+
+#include "dali/kernels/common/utils.h"
+#include "dali/kernels/scratch.h"
+// #include "dali/pipeline/data/tensor_list.h"
+#include "dali/test/tensor_test_utils.h"
+#include "dali/test/test_tensors.h"
+#include "dali/kernels/imgproc/convolution/separable_convolution.h"
+
+
+namespace dali {
+namespace kernels {
+
+
+template <typename T>
+struct CyclicPixelWrapperTest: public ::testing::Test {
+};
+
+TYPED_TEST_SUITE_P(CyclicPixelWrapperTest);
+
+using CyclicPixelWrapperValues =
+    ::testing::Types<std::integral_constant<int, 1>, std::integral_constant<int, 3>>;
+
+TYPED_TEST_P(CyclicPixelWrapperTest, FillAndCycle) {
+  constexpr int size = 6;
+  constexpr int num_channels = TypeParam::value;
+  int tmp_buffer[size * num_channels];
+  int input_buffer[size * num_channels];
+  for (int i = 0; i < size * num_channels; i++) {
+    input_buffer[i] = i;
+    tmp_buffer[i] = -1;
+  }
+  CyclicPixelWrapper<int> cpw(tmp_buffer, size, num_channels);
+  EXPECT_EQ(0, cpw.Size());
+  for (int i = 0; i < size; i++) {
+    cpw.PushPixel(input_buffer + i * num_channels);
+    EXPECT_EQ(tmp_buffer + i * num_channels, cpw.GetPixelOffset(i));
+    for (int c = 0; c < num_channels; c++) {
+      EXPECT_EQ(input_buffer[i * num_channels + c], cpw.GetPixelOffset(i)[c]);
+    }
+  }
+  for (int i = 0; i < size; i++) {
+    cpw.PopPixel();
+    cpw.PushPixel(input_buffer + i * num_channels);
+    for (int j = 0; j < size; j++) {
+      // we're starting at i + 1 as we did already one Pop & Push operation
+      int element = (i + 1 + j) % size;
+      EXPECT_EQ(tmp_buffer + element * num_channels, cpw.GetPixelOffset(j));
+      for (int c = 0; c < num_channels; c++) {
+        EXPECT_EQ(input_buffer[element * num_channels + c], cpw.GetPixelOffset(j)[c]);
+      }
+    }
+  }
+}
+
+void baseline_dot(span<int> result, span<const int> input, span<const int> window, int in_offset) {
+  int num_channels = result.size();
+  int num_elements = window.size();
+  ASSERT_EQ(input.size(), num_channels * num_elements);
+  for (int c = 0; c < num_channels; c++) {
+    result[c] = 0;
+    for (int i = 0; i < num_elements; i++) {
+      int in_elem = (i + in_offset) % num_elements;
+      result[c] += window[i] * input[in_elem * num_channels + c];
+    }
+  }
+}
+
+TYPED_TEST_P(CyclicPixelWrapperTest, DotProduct) {
+  constexpr int size = 6;
+  constexpr int num_channels = TypeParam::value;;
+  int tmp_buffer[size * num_channels];
+  int input_buffer[size * num_channels];
+  int window[size];
+  for (int i = 0; i < size * num_channels; i++) {
+    input_buffer[i] = i;
+    tmp_buffer[i] = -1;
+  }
+  for (int i = 0; i < size; i++) {
+    window[i] = i;
+  }
+  int baseline[num_channels], result[num_channels];
+  for (int c = 0; c < num_channels; c++) {
+    baseline[c] = 0;
+    for (int i = 0; i < size; i++) {
+      baseline[c] += window[i] * input_buffer[i * num_channels + c];
+    }
+  }
+
+  CyclicPixelWrapper<int> cpw(tmp_buffer, size, num_channels);
+  for (int i = 0; i < size; i++) {
+    cpw.PushPixel(input_buffer + i * num_channels);
+  }
+  cpw.CalculateDot(result, window);
+  baseline_dot(make_span(baseline), make_span(input_buffer), make_span(window), 0);
+  for (int c = 0; c < num_channels; c++) {
+    EXPECT_EQ(baseline[c], result[c]);
+  }
+  for (int i = 0; i < size; i++) {
+    cpw.PopPixel();
+    cpw.PushPixel(input_buffer + i * num_channels);
+    cpw.CalculateDot(result, window);
+    // again we start here at i + 1 offset
+    baseline_dot(make_span(baseline), make_span(input_buffer), make_span(window), i + 1);
+    for (int c = 0; c < num_channels; c++) {
+      EXPECT_EQ(baseline[c], result[c]);
+    }
+  }
+}
+
+REGISTER_TYPED_TEST_SUITE_P(CyclicPixelWrapperTest, FillAndCycle, DotProduct);
+
+INSTANTIATE_TYPED_TEST_SUITE_P(CyclicPixelWrapper, CyclicPixelWrapperTest, CyclicPixelWrapperValues);
+
+
+TEST(SeparableConvolutionTest, OneAxisTest) {
+  constexpr int window_size = 3;
+  constexpr int num_channels = 3;
+  constexpr int input_len = 11;
+  TestTensorList<float, 1> kernel_window;
+  kernel_window.reshape(uniform_list_shape<1>(1, {window_size}));
+  TestTensorList<uint8_t, 2> input;
+  input.reshape(uniform_list_shape<2>(1, {input_len, num_channels}));
+  TestTensorList<uint8_t, 2> padded_input;
+  padded_input.reshape(uniform_list_shape<2>(1, {input_len + window_size - 1, num_channels}));
+  TestTensorList<float, 2> output;
+  output.reshape(uniform_list_shape<2>(1, {input_len, num_channels}));
+
+  using Kernel1d = SeparableConvolution<float, uint8_t, float, 2>;
+  using Kernel2d = SeparableConvolution<float, uint8_t, float, 3>;
+  using Kernel3d = SeparableConvolution<float, uint8_t, float, 4>;
+
+  KernelContext ctx;
+  Kernel1d k1d;
+
+  auto out = output.cpu()[0];
+  auto in = input.cpu()[0];
+  auto k_win = kernel_window.cpu()[0];
+  // almost box filter, with raised center
+  for (int i = 0; i < window_size; i++) {
+    if (i < window_size / 2) {
+      k_win.data[i] = 1;
+    } else if (i == window_size / 2) {
+      k_win.data[i] = 2;
+    } else {
+      k_win.data[i] = 1;
+    }
+  }
+  for (int i = 0; i < input_len * num_channels; i++) {
+    in.data[i] = 0;
+  }
+  for (int c = 0; c < num_channels; c++) {
+    in.data[(input_len / 2) * num_channels + c] = 10;
+  }
+
+  auto req = k1d.Setup(ctx, in, k_win, 0);
+  // this is painful
+  ScratchpadAllocator scratch_alloc;
+  scratch_alloc.Reserve(req.scratch_sizes);
+  auto scratchpad = scratch_alloc.GetScratchpad();
+  ctx.scratchpad = &scratchpad;
+  k1d.Run(ctx, out, in, k_win, 0, 1);
+  for (int i = 0; i < input_len; i++) {
+    std::cout << "{ ";
+    for (int c = 0; c< num_channels; c++) {
+      std::cout << out.data[i * num_channels + c] << ", ";
+    }
+    std::cout << " }\n";
+  }
+  std::cout << std::endl;
+  Kernel2d k2d;
+  Kernel3d k3d;
+}
+
+
+}  // namespace kernels
+}  // namespace dali
