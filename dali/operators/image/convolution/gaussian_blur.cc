cb2a695eab34ca22875b2f7d487f2cd0f0ba0499
Krzysztof Lecki <klecki@nvidia.com>
2020-05-22 20:33:06 +0200

    Initial convolution work [WIP]
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>
    
    Works for one axis
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>
    
    Remove some printfs
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>
    
    Remove more printfs
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>
    
    Some more cleanup
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>
    
    Rename
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>
    
    Scaffolding for GaussianBlur is working
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>
    
    Kernel rename
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>
    
    Reworked the convolution
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

---
+++ dali/operators/image/convolution/gaussian_blur.cc
@@ -0,0 +1,44 @@
+// Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "dali/operators/image/convolution/gaussian_blur.h"
+
+namespace dali {
+
+DALI_SCHEMA(GaussianBlur)
+    .DocStr(R"code(Apply Gaussian Blur to the input.
+Separable convolution with Gaussian Kernel is used to calculate the output.
+
+Every axis can have distinct sigma value specified, the channels are not considered an axis.
+
+User can specify either ``sigma`` that will be used uniformly for all axes
+or ``sigma_0``, ``sigma_1``, ``sigma_2``. ``sigma_i`` will be used for corresponding axis ``i``.
+
+In case of sequences of ``FHWC`` layout, the frame dimension is skipped, ``sigma_0``
+is used for ``H`` and ``sigma_1`` for ``W``.
+)code")
+    .NumInput(1)
+    .NumOutput(1)
+    .AllowSequences()
+    .SupportVolumetric()
+    .AddOptionalArg<float>("sigma", R"code()code", 1.f, true)
+    .AddOptionalArg<float>("sigma_0", R"code()code", nullptr, true)
+    .AddOptionalArg<float>("sigma_1", R"code()code", nullptr, true)
+    .AddOptionalArg<float>("sigma_2", R"code()code", nullptr, true);
+
+// constexpr int GaussianBlur<CPUBackend>::kMaxDim = 3;
+
+// DALI_REGISTER_OPERATOR(GaussianBlur, GaussianBlur<CPUBackend>, CPU);
+
+}  // namespace dali
