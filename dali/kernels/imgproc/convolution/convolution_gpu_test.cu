cb60dfbca406dc75cf42e7328dbc9cbe7bd20495
Krzysztof Lecki <klecki@nvidia.com>
2020-08-21 17:47:04 +0200

    Better, 3D outer and inner works, needs to fix middle
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/kernels/imgproc/convolution/convolution_gpu_test.cu
+++ dali/kernels/imgproc/convolution/convolution_gpu_test.cu
@@ -102,7 +102,7 @@ struct ConvolutionGpuKernelTest : public ::testing::Test {
     // ConstantFill(in_, 0);
 
     std::mt19937 rng;
-    UniformRandomFill(baseline_in_, rng, 0, 255);
+    UniformRandomFill(baseline_in_, rng, 0, 64);
     // SequentialFill(baseline_in_);
     in_ = input_.gpu();
 
@@ -143,7 +143,7 @@ struct ConvolutionGpuKernelTest : public ::testing::Test {
     auto out_cpu_ = output_.cpu();
     // Check(out_cpu_, baseline_out_);
 
-    double eps = 1e-2;
+    double eps = 1e-1;
     Check(out_cpu_, baseline_out_, EqualEps(eps));
     // printf("CUTLASS\n\n");
     // print_mat(out_cpu_[0].shape[0], out_cpu_[0].shape[1] * out_cpu_[0].shape[2], out_cpu_[0].data);
@@ -166,8 +166,8 @@ struct ConvolutionGpuKernelTest : public ::testing::Test {
   // const TensorListShape<> shape_noch_ = {{64, 64, 64}};
   // const TensorListShape<> shape_ch_ = {{64, 8, 48, 3}};
   // const TensorListShape<> shape_noch_ = {{32, 32, 48}};
-  const TensorListShape<> shape_ch_ = {{64, 64, 64, 3}, {164, 164, 164, 3}, {512, 512, 512, 3}, {64, 128, 512, 3}, {64, 512, 128, 3}, {512, 512, 512, 5}};
-  const TensorListShape<> shape_noch_ = {{64, 64, 64}, {164, 164, 164}, {512, 512, 512}, {64, 128, 512}, {64, 512, 128}, {123, 123, 123}};
+  const TensorListShape<> shape_ch_ = {{64, 64, 64, 3}, {164, 164, 164, 3}, {12, 12, 12, 3}, {4, 200, 180, 3}, {200, 4, 180, 3}, {75, 75, 75, 5}};
+  const TensorListShape<> shape_noch_ = {{64, 64, 64}, {164, 164, 164}, {12, 12, 12}, {4, 200, 180}, {200, 4, 180}, {75, 75, 75}};
   const TensorListShape<1> shape_window = uniform_list_shape(shape_ch_.num_samples(), TensorShape<1>{T::window_size});
 };
 
@@ -176,13 +176,54 @@ TYPED_TEST_SUITE_P(ConvolutionGpuKernelTest);
 // template <int ndim_, bool has_channels_, int axis_, int window_size_, typename InType_>
 using ConvolutionTestValues = ::testing::Types<
     convolution_params<2, false, 0, 3, float>,
-    convolution_params<2, false, 1, 3, float>,
-    convolution_params<3, true, 0, 3, float>,
-    convolution_params<3, true, 1, 3, float>,
     convolution_params<2, false, 0, 15, float>,
+    convolution_params<2, false, 0, 51, float>,
+    convolution_params<2, false, 0, 101, float>,
+
+    convolution_params<2, false, 1, 3, float>,
     convolution_params<2, false, 1, 15, float>,
+    convolution_params<2, false, 1, 51, float>,
+    convolution_params<2, false, 1, 101, float>,
+
+    convolution_params<3, true, 0, 3, float>,
     convolution_params<3, true, 0, 15, float>,
-    convolution_params<3, true, 1, 15, float>
+    convolution_params<3, true, 0, 51, float>,
+    convolution_params<3, true, 0, 101, float>,
+
+    convolution_params<3, true, 1, 3, float>,
+    convolution_params<3, true, 1, 15, float>,
+    convolution_params<3, true, 1, 51, float>,
+    convolution_params<3, true, 1, 101, float>,
+
+    convolution_params<3, false, 0, 3, float>,
+    convolution_params<3, false, 0, 15, float>,
+    convolution_params<3, false, 0, 51, float>,
+    convolution_params<3, false, 0, 101, float>,
+
+    convolution_params<3, false, 1, 3, float>,
+    convolution_params<3, false, 1, 15, float>,
+    convolution_params<3, false, 1, 51, float>,
+    convolution_params<3, false, 1, 101, float>,
+
+    convolution_params<3, false, 2, 3, float>,
+    convolution_params<3, false, 2, 15, float>,
+    convolution_params<3, false, 2, 51, float>,
+    convolution_params<3, false, 2, 101, float>,
+
+    convolution_params<4, true, 0, 3, float>,
+    convolution_params<4, true, 0, 15, float>,
+    convolution_params<4, true, 0, 51, float>,
+    convolution_params<4, true, 0, 101, float>,
+
+    convolution_params<4, true, 1, 3, float>,
+    convolution_params<4, true, 1, 15, float>,
+    convolution_params<4, true, 1, 51, float>,
+    convolution_params<4, true, 1, 101, float>,
+
+    convolution_params<4, true, 2, 3, float>,
+    convolution_params<4, true, 2, 15, float>,
+    convolution_params<4, true, 2, 51, float>,
+    convolution_params<4, true, 2, 101, float>
                                                     >;
 // convolution_params<1, false, 0, 1, uint8_t>,
                                               //  convolution_params<1, false, 0, 3, uint8_t>,
