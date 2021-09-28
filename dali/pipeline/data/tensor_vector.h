46e051cf8e24f8bd47499ab3d7e8db62f0051652
Krzysztof Lecki <klecki@nvidia.com>
2021-09-28 20:58:45 +0200

    Prototyp i wydmuszka
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/pipeline/data/tensor_vector.h
+++ dali/pipeline/data/tensor_vector.h
@@ -29,6 +29,10 @@
 
 namespace dali {
 
+
+template <typename Backend>
+class TensorVector : public TensorBatch<Backend> {};
+
 /**
  * @brief Merges TensorList<Backend> and std::vector<std::shared_ptr<Tensor<Backend>>> APIs
  * providing an uniform way of handling a collection/batch of tensors_.
@@ -37,196 +41,196 @@ namespace dali {
  *
  * @tparam Backend
  */
-template <typename Backend>
-class DLL_PUBLIC TensorVector {
- public:
-  TensorVector();
+// template <typename Backend>
+// class DLL_PUBLIC TensorVector {
+//  public:
+//   TensorVector();
 
-  explicit TensorVector(int batch_size);
+//   explicit TensorVector(int batch_size);
 
-  explicit TensorVector(std::shared_ptr<TensorList<Backend>> tl);
+//   explicit TensorVector(std::shared_ptr<TensorList<Backend>> tl);
 
-  TensorVector(const TensorVector &) = delete;
-  TensorVector &operator=(const TensorVector &) = delete;
+//   TensorVector(const TensorVector &) = delete;
+//   TensorVector &operator=(const TensorVector &) = delete;
 
-  DLL_PUBLIC TensorVector<Backend>(TensorVector<Backend> &&other) noexcept;
+//   DLL_PUBLIC TensorVector<Backend>(TensorVector<Backend> &&other) noexcept;
 
-  Tensor<Backend> &operator[](size_t pos) {
-    return *(tensors_[pos]);
-  }
+//   Tensor<Backend> &operator[](size_t pos) {
+//     return *(tensors_[pos]);
+//   }
 
-  const Tensor<Backend> &operator[](size_t pos) const {
-    return *(tensors_[pos]);
-  }
+//   const Tensor<Backend> &operator[](size_t pos) const {
+//     return *(tensors_[pos]);
+//   }
 
-  auto tensor_handle(size_t pos) {
-    return tensors_[pos];
-  }
+//   auto tensor_handle(size_t pos) {
+//     return tensors_[pos];
+//   }
 
-  auto tensor_handle(size_t pos) const {
-    return tensors_[pos];
-  }
+//   auto tensor_handle(size_t pos) const {
+//     return tensors_[pos];
+//   }
 
-  auto begin() noexcept {
-    return tensors_.begin();
-  }
+//   auto begin() noexcept {
+//     return tensors_.begin();
+//   }
 
-  auto begin() const noexcept {
-    return tensors_.begin();
-  }
+//   auto begin() const noexcept {
+//     return tensors_.begin();
+//   }
 
-  auto cbegin() const noexcept {
-    return tensors_.cbegin();
-  }
+//   auto cbegin() const noexcept {
+//     return tensors_.cbegin();
+//   }
 
-  auto end() noexcept {
-    return tensors_.end();
-  }
+//   auto end() noexcept {
+//     return tensors_.end();
+//   }
 
-  auto end() const noexcept {
-    return tensors_.end();
-  }
+//   auto end() const noexcept {
+//     return tensors_.end();
+//   }
 
-  auto cend() const noexcept {
-    return tensors_.cend();
-  }
+//   auto cend() const noexcept {
+//     return tensors_.cend();
+//   }
 
-  auto size() const noexcept {
-    return curr_tensors_size_;
-  }
+//   auto size() const noexcept {
+//     return curr_tensors_size_;
+//   }
 
-  size_t ntensor() const noexcept {
-    return curr_tensors_size_;
-  }
+//   size_t ntensor() const noexcept {
+//     return curr_tensors_size_;
+//   }
 
-  int sample_dim() const {
-    return IsContiguous() ? tl_->sample_dim() : ntensor() ? tensors_[0]->shape().size() : 0;
-  }
+//   int sample_dim() const {
+//     return IsContiguous() ? tl_->sample_dim() : ntensor() ? tensors_[0]->shape().size() : 0;
+//   }
 
-  size_t nbytes() const noexcept;
+//   size_t nbytes() const noexcept;
 
-  size_t capacity() const noexcept;
+//   size_t capacity() const noexcept;
 
-  TensorListShape<> shape() const;
+//   TensorListShape<> shape() const;
 
-  const TensorShape<> &tensor_shape(int idx) const {
-    return tensors_[idx]->shape();
-  }
+//   const TensorShape<> &tensor_shape(int idx) const {
+//     return tensors_[idx]->shape();
+//   }
 
-  const void *raw_tensor(int idx) const {
-    return tensors_[idx]->raw_data();
-  }
+//   const void *raw_tensor(int idx) const {
+//     return tensors_[idx]->raw_data();
+//   }
 
-  void* raw_mutable_tensor(int idx) {
-    return tensors_[idx]->raw_mutable_data();
-  }
+//   void* raw_mutable_tensor(int idx) {
+//     return tensors_[idx]->raw_mutable_data();
+//   }
 
-  DLL_PUBLIC void Resize(const TensorListShape<> &new_shape) {
-    return Resize(new_shape, type());
-  }
+//   DLL_PUBLIC void Resize(const TensorListShape<> &new_shape) {
+//     return Resize(new_shape, type());
+//   }
 
-  DLL_PUBLIC void Resize(const TensorListShape<> &new_shape, DALIDataType new_type);
+//   DLL_PUBLIC void Resize(const TensorListShape<> &new_shape, DALIDataType new_type);
 
-  /**
-   * Change the number of tensors in the TensorVector, without the need of
-   * specifying the shape of every such tensor. When setting the new size,
-   * this function will retain the shapes that already exist. New tensors
-   * are given a shape of 0-volume and appropriate dimension.
-   * @param new_size
-   */
-  void SetSize(int new_size);
+//   /**
+//    * Change the number of tensors in the TensorVector, without the need of
+//    * specifying the shape of every such tensor. When setting the new size,
+//    * this function will retain the shapes that already exist. New tensors
+//    * are given a shape of 0-volume and appropriate dimension.
+//    * @param new_size
+//    */
+//   void SetSize(int new_size);
 
-  void set_type(DALIDataType new_type);
+//   void set_type(DALIDataType new_type);
 
-  template <typename T>
-  void set_type() {
-    set_type(TypeTable::GetTypeID<T>());
-  }
+//   template <typename T>
+//   void set_type() {
+//     set_type(TypeTable::GetTypeID<T>());
+//   }
 
-  DALIDataType type() const;
+//   DALIDataType type() const;
 
-  const TypeInfo &type_info() const;
+//   const TypeInfo &type_info() const;
 
-  /** @brief Set uniform layout for all samples in the list */
-  void SetLayout(const TensorLayout &layout);
+//   /** @brief Set uniform layout for all samples in the list */
+//   void SetLayout(const TensorLayout &layout);
 
-  TensorLayout GetLayout() const;
+//   TensorLayout GetLayout() const;
 
-  const DALIMeta &GetMeta(int idx) const;
+//   const DALIMeta &GetMeta(int idx) const;
 
-  void SetMeta(int idx, const DALIMeta &meta);
+//   void SetMeta(int idx, const DALIMeta &meta);
 
-  void set_pinned(bool pinned);
+//   void set_pinned(bool pinned);
 
-  bool is_pinned() const;
+//   bool is_pinned() const;
 
-  /**
-   * @brief Reserve as contiguous tensor list internally
-   */
-  void reserve(size_t total_bytes);
+//   /**
+//    * @brief Reserve as contiguous tensor list internally
+//    */
+//   void reserve(size_t total_bytes);
 
-  /**
-   * @brief Reserve as vector of `batch_size` tensors internally
-   */
-  void reserve(size_t bytes_per_sample, int batch_size);
+//   /**
+//    * @brief Reserve as vector of `batch_size` tensors internally
+//    */
+//   void reserve(size_t bytes_per_sample, int batch_size);
 
-  /**
-   * @brief If the TensorVector is backed by TensorList (contiguous memory)
-   */
-  bool IsContiguous() const noexcept;
+//   /**
+//    * @brief If the TensorVector is backed by TensorList (contiguous memory)
+//    */
+//   bool IsContiguous() const noexcept;
 
-  /**
-   * @brief Set the current state if further calls like Resize() or set_type
-   *        should use TensorList or std::vector<Tensor> as backing memory
-   */
-  void SetContiguous(bool contiguous);
+//   /**
+//    * @brief Set the current state if further calls like Resize() or set_type
+//    *        should use TensorList or std::vector<Tensor> as backing memory
+//    */
+//   void SetContiguous(bool contiguous);
 
-  void Reset();
+//   void Reset();
 
-  template <typename SrcBackend>
-  void Copy(const TensorList<SrcBackend> &in_tl, cudaStream_t stream);
+//   template <typename SrcBackend>
+//   void Copy(const TensorList<SrcBackend> &in_tl, cudaStream_t stream);
 
-  template <typename SrcBackend>
-  void Copy(const TensorVector<SrcBackend> &in_tv, cudaStream_t stream);
+//   template <typename SrcBackend>
+//   void Copy(const TensorVector<SrcBackend> &in_tv, cudaStream_t stream);
 
-  void ShareData(TensorList<Backend> *in_tl);
+//   void ShareData(TensorList<Backend> *in_tl);
 
-  void ShareWith(TensorList<Backend> *in_tl) const;
+//   void ShareWith(TensorList<Backend> *in_tl) const;
 
-  void ShareData(TensorVector<Backend> *tv);
+//   void ShareData(TensorVector<Backend> *tv);
 
-  TensorVector<Backend> &operator=(TensorVector<Backend> &&other) noexcept;
+//   TensorVector<Backend> &operator=(TensorVector<Backend> &&other) noexcept;
 
-  void UpdateViews();
+//   void UpdateViews();
 
-  shared_ptr<TensorList<Backend>> AsTensorList(bool check_contiguity = true);
+//   shared_ptr<TensorList<Backend>> AsTensorList(bool check_contiguity = true);
 
- private:
-  enum class State { contiguous, noncontiguous };
+//  private:
+//   enum class State { contiguous, noncontiguous };
 
-  struct ViewRefDeleter {
-    void operator()(void*) { --*ref; }
-    std::atomic<int> *ref;
-  };
+//   struct ViewRefDeleter {
+//     void operator()(void*) { --*ref; }
+//     std::atomic<int> *ref;
+//   };
 
-  void resize_tensors(int size);
+//   void resize_tensors(int size);
 
-  void update_view(int idx);
+//   void update_view(int idx);
 
-  std::atomic<int> views_count_;
-  std::vector<std::shared_ptr<Tensor<Backend>>> tensors_;
-  size_t curr_tensors_size_;
-  std::shared_ptr<TensorList<Backend>> tl_;
-  State state_ = State::noncontiguous;
-  // pinned status and type info should be uniform
-  bool pinned_ = true;
-  TypeInfo type_{};
+//   std::atomic<int> views_count_;
+//   std::vector<std::shared_ptr<Tensor<Backend>>> tensors_;
+//   size_t curr_tensors_size_;
+//   std::shared_ptr<TensorList<Backend>> tl_;
+//   State state_ = State::noncontiguous;
+//   // pinned status and type info should be uniform
+//   bool pinned_ = true;
+//   TypeInfo type_{};
 
-  // So we can access the members of other TensorVectors
-  // with different template types
-  template <typename InBackend>
-  friend class TensorVector;
-};
+//   // So we can access the members of other TensorVectors
+//   // with different template types
+//   template <typename InBackend>
+//   friend class TensorVector;
+// };
 
 }  // namespace dali
 
--- dali/pipeline/workspace/workspace.h
+++ dali/pipeline/workspace/workspace.h
@@ -55,12 +55,13 @@ class ArgumentWorkspace {
     argument_inputs_[arg_name] = { std::move(input), false };
   }
 
-  void AddArgumentInput(const std::string &arg_name, shared_ptr<TensorList<CPUBackend>> input) {
-    argument_inputs_[arg_name] = {
-      std::make_shared<TensorVector<CPUBackend>>(std::move(input)),
-      true
-    };
-  }
+  // TODO
+  // void AddArgumentInput(const std::string &arg_name, shared_ptr<TensorList<CPUBackend>> input) {
+  //   argument_inputs_[arg_name] = {
+  //     std::make_shared<TensorVector<CPUBackend>>(std::move(input)),
+  //     true
+  //   };
+  // }
 
   const TensorVector<CPUBackend>& ArgumentInput(const std::string &arg_name) const {
     auto it = argument_inputs_.find(arg_name);
