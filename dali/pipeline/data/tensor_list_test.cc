72145399c96338d9f586de28d23b0b7b7fa5cb88
Krzysztof Lecki <klecki@nvidia.com>
2021-10-04 17:57:27 +0200

    Api removal
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/pipeline/data/tensor_list_test.cc
+++ dali/pipeline/data/tensor_list_test.cc
@@ -81,7 +81,7 @@ class TensorListTest : public DALITest {
     for (int i = 0; i < num_tensor; ++i) {
       ASSERT_NE(tensor_list->template mutable_tensor<float>(i), nullptr);
       ASSERT_EQ(tensor_list->tensor_shape(i), shape[i]);
-      ASSERT_EQ(tensor_list->tensor_offset(i), (*offsets)[i]);
+      // ASSERT_EQ(tensor_list->tensor_offset(i), (*offsets)[i]);
     }
   }
 };
@@ -133,7 +133,7 @@ TYPED_TEST(TensorListTest, TestGetTypeSizeBytes) {
 
   for (int i = 0; i < num_tensor; ++i) {
     ASSERT_NE(tl.raw_tensor(i), nullptr);
-    ASSERT_EQ(tl.tensor_offset(i), offsets[i]);
+    // ASSERT_EQ(tl.tensor_offset(i), offsets[i]);
   }
 }
 
@@ -172,7 +172,7 @@ TYPED_TEST(TensorListTest, TestGetSizeTypeBytes) {
 
   for (int i = 0; i < num_tensor; ++i) {
     ASSERT_NE(tl.template tensor<float>(i), nullptr);
-    ASSERT_EQ(tl.tensor_offset(i), offsets[i]);
+    // ASSERT_EQ(tl.tensor_offset(i), offsets[i]);
   }
 }
 
@@ -296,7 +296,7 @@ TYPED_TEST(TensorListTest, TestMultipleZeroSizeResize) {
   for (int i = 0; i < num_tensor; ++i) {
     ASSERT_EQ(tensor_list.template tensor<float>(i), nullptr);
     ASSERT_EQ(tensor_list.tensor_shape(i), TensorShape<>{ 0 });
-    ASSERT_EQ(tensor_list.tensor_offset(i), 0);
+    // ASSERT_EQ(tensor_list.tensor_offset(i), 0);
   }
 }
 
@@ -316,7 +316,7 @@ TYPED_TEST(TensorListTest, TestFakeScalarResize) {
   for (int i = 0; i < num_scalar; ++i) {
     ASSERT_NE(tensor_list.raw_tensor(i), nullptr);
     ASSERT_EQ(tensor_list.tensor_shape(i), TensorShape<>{1});  // {1} on purpose
-    ASSERT_EQ(tensor_list.tensor_offset(i), i);
+    // ASSERT_EQ(tensor_list.tensor_offset(i), i);
   }
 }
 
@@ -336,7 +336,7 @@ TYPED_TEST(TensorListTest, TestTrueScalarResize) {
   for (int i = 0; i < num_scalar; ++i) {
     ASSERT_NE(tensor_list.raw_tensor(i), nullptr);
     ASSERT_EQ(tensor_list.tensor_shape(i), TensorShape<>{});
-    ASSERT_EQ(tensor_list.tensor_offset(i), i);
+    // ASSERT_EQ(tensor_list.tensor_offset(i), i);
   }
 }
 
@@ -381,7 +381,7 @@ TYPED_TEST(TensorListTest, TestMultipleResize) {
   for (int i = 0; i < num_tensor; ++i) {
     ASSERT_NE(tensor_list.raw_tensor(i), nullptr);
     ASSERT_EQ(tensor_list.tensor_shape(i), shape[i]);
-    ASSERT_EQ(tensor_list.tensor_offset(i), offsets[i]);
+    // ASSERT_EQ(tensor_list.tensor_offset(i), offsets[i]);
   }
 }
 TYPED_TEST(TensorListTest, TestCopy) {
@@ -441,7 +441,7 @@ TYPED_TEST(TensorListTest, TestTypeChangeSameSize) {
   for (size_t i = 0; i < tensor_list.num_samples(); ++i) {
     ASSERT_EQ(ptrs[i], tensor_list.raw_tensor(i));
     ASSERT_EQ(tensor_list.tensor_shape(i), shape[i]);
-    ASSERT_EQ(tensor_list.tensor_offset(i), offsets[i]);
+    // ASSERT_EQ(tensor_list.tensor_offset(i), offsets[i]);
   }
 
   // No memory allocation should have occurred
@@ -468,7 +468,7 @@ TYPED_TEST(TensorListTest, TestTypeChangeSmaller) {
   for (size_t i = 0; i < tensor_list.num_samples(); ++i) {
     ASSERT_EQ(unsafe_raw_data(tensor_list), base_ptr);
     ASSERT_EQ(tensor_list.tensor_shape(i), shape[i]);
-    ASSERT_EQ(tensor_list.tensor_offset(i), offsets[i]);
+    // ASSERT_EQ(tensor_list.tensor_offset(i), offsets[i]);
   }
 
   // nbytes should have reduced by a factor of 4
@@ -493,7 +493,7 @@ TYPED_TEST(TensorListTest, TestTypeChangeLarger) {
   ASSERT_EQ(tensor_list.num_samples(), shape.size());
   for (size_t i = 0; i < tensor_list.num_samples(); ++i) {
     ASSERT_EQ(tensor_list.tensor_shape(i), shape[i]);
-    ASSERT_EQ(tensor_list.tensor_offset(i), offsets[i]);
+    // ASSERT_EQ(tensor_list.tensor_offset(i), offsets[i]);
   }
 
   // nbytes should have increased by a factor of 2
@@ -540,7 +540,7 @@ TYPED_TEST(TensorListTest, TestShareData) {
   for (size_t i = 0; i < tensor_list.num_samples(); ++i) {
     ASSERT_EQ(tensor_list.raw_tensor(i), tensor_list2.raw_tensor(i));
     ASSERT_EQ(tensor_list2.tensor_shape(i), shape[i]);
-    ASSERT_EQ(tensor_list2.tensor_offset(i), offsets[i]);
+    // ASSERT_EQ(tensor_list2.tensor_offset(i), offsets[i]);
   }
 
   // Trigger allocation through buffer API, verify we cannot do that
