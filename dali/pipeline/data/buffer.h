043f089350a82f1c43a83cd87bfad7bfbd236fba
Krzysztof Lecki <klecki@nvidia.com>
2021-11-05 18:26:21 +0100

    We really need the unsafe sample owner to work,
    TODO Getting as Tensor as well
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/pipeline/data/buffer.h
+++ dali/pipeline/data/buffer.h
@@ -335,17 +335,17 @@ class DLL_PUBLIC Buffer {
    */
   inline void resize(int64_t new_elements, DALIDataType new_type_id) {
     if (!is_reallocation(new_elements, new_type_id)) {
-      std::cout << make_string(">>> Buffer resize(", new_elements, ", ", new_type_id,
-                               ") - current: ", size_, " elements of type ", type_.id(),
-                               " - no reallocation.") << std::endl;
+      // std::cout << make_string(">>> Buffer resize(", new_elements, ", ", new_type_id,
+      //                          ") - current: ", size_, " elements of type ", type_.id(),
+      //                          " - no reallocation.") << std::endl;
       // no reallocation, adjust relevant metadata
       size_ = new_elements;
       if (type_.id() != new_type_id) {
         type_ = TypeTable::GetTypeInfo(new_type_id);
       }
-      std::cout << make_string("<<< Buffer after resize: elements: ", size_,
-                               ", bytes: ", num_bytes_, ", type: ", type_.id(), ".")
-                << std::endl;
+      // std::cout << make_string("<<< Buffer after resize: elements: ", size_,
+      //                          ", bytes: ", num_bytes_, ", type: ", type_.id(), ".")
+      //           << std::endl;
       return;
     }
     // We try to make the buffer bigger while sharing the data - this is prohibited.
@@ -362,16 +362,16 @@ class DLL_PUBLIC Buffer {
     size_t new_num_bytes = new_alloc_size(new_elements, new_type_id);
     if (new_num_bytes > num_bytes_) {
 
-      std::cout << make_string(">>> Buffer resize(", new_elements, ", ", new_type_id,
-                               ") - current: ", size_, " elements of type ", type_.id(),
-                               " - reallocation to: ", new_num_bytes, " bytes.") << std::endl;
+      // std::cout << make_string(">>> Buffer resize(", new_elements, ", ", new_type_id,
+      //                          ") - current: ", size_, " elements of type ", type_.id(),
+      //                          " - reallocation to: ", new_num_bytes, " bytes.") << std::endl;
       size_t grow = num_bytes_ * growth_factor_;
       if (grow > new_num_bytes) new_num_bytes = grow;
       reserve(new_num_bytes);
     } else {
-      std::cout << make_string(">>> Buffer resize(", new_elements, ", ", new_type_id,
-                               ") - current: ", size_, " elements of type ", type_.id(),
-                               " - shrinking to: ", new_num_bytes, " bytes.") << std::endl;
+      // std::cout << make_string(">>> Buffer resize(", new_elements, ", ", new_type_id,
+      //                          ") - current: ", size_, " elements of type ", type_.id(),
+      //                          " - shrinking to: ", new_num_bytes, " bytes.") << std::endl;
       data_.reset();
       num_bytes_ = 0;
       // TODO: Exception safety?
@@ -383,9 +383,9 @@ class DLL_PUBLIC Buffer {
     if (type_.id() != new_type_id) {
       type_ = TypeTable::GetTypeInfo(new_type_id);
     }
-    std::cout << make_string("<<< Buffer after resize: elements: ", size_, ", bytes: ", num_bytes_,
-                             ", type: ", type_.id(), ".")
-              << std::endl;
+    // std::cout << make_string("<<< Buffer after resize: elements: ", size_, ", bytes: ", num_bytes_,
+    //                          ", type: ", type_.id(), ".")
+    //          << std::endl;
   }
 
   inline void reserve(size_t new_num_bytes) {
