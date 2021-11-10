e99f6c4c6075d1cd81f7b856fa43529769f9b7cf
Krzysztof Lecki <klecki@nvidia.com>
2021-11-10 20:08:35 +0100

    COnstant fixup
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/operators/generic/constant.cc
+++ dali/operators/generic/constant.cc
@@ -74,9 +74,11 @@ namespace {
 template <typename Dst, typename Src>
 void FillTensorVector(
   TensorVector<CPUBackend> &dst, const TensorListShape<> &shape, const std::vector<Src> &src) {
-  dst.SetContiguous(false);
+  // dst.SetContiguous(false);
   dst.set_type<Dst>();
-  dst.Resize(shape);
+  dst.SetSize(shape.num_samples());
+  SampleAccessLock lock(dst);
+  dst[0].Resize(shape[0]);
   assert(is_uniform(shape));
   int64_t n = shape[0].num_elements();
   assert(src.size() == static_cast<size_t>(n) || src.size() == 1);
