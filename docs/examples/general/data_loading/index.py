44379fd2d53d41d0650e66aa9ab3e1f952a0f0ea
Krzysztof Lecki <klecki@nvidia.com>
2022-04-01 19:11:04 +0200

    WIP
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- docs/examples/general/data_loading/index.py
+++ docs/examples/general/data_loading/index.py
@@ -10,10 +10,23 @@ doc(title="Data Loading",
             "parallel_external_source_fork.ipynb",
             op_reference("fn.external_source",
                          "How to use parallel mode for external source in fork mode")),
-        "dataloading_lmdb.ipynb",
-        "dataloading_recordio.ipynb",
-        "dataloading_tfrecord.ipynb",
-        "dataloading_webdataset.ipynb",
-        "coco_reader.ipynb",
-        "numpy_reader.ipynb",
+        doc_entry("dataloading_lmdb.ipynb", [
+            op_reference("fn.readers.caffe", "Loading Caffe and Caffe 2 LMBD data"),
+            op_reference("fn.readers.caffe2", "Loading Caffe and Caffe 2 LMBD data")
+        ]),
+        doc_entry("dataloading_recordio.ipynb",
+                  op_reference("fn.readers.mxnet", "Loading MXNet recordIO data")),
+        doc_entry("dataloading_tfrecord.ipynb",
+                  op_reference("fn.readers.tfrecord", "Loading TensorFlow TFRecord data")),
+        doc_entry("dataloading_webdataset.ipynb",
+                  op_reference("fn.readers.webdataset",
+                               "Loading data stored in Webdataset format")),
+        doc_entry("coco_reader.ipynb",
+                  op_reference("fn.readers.coco", "Loading COCO dataset or its subsets")),
+        doc_entry(
+            "numpy_reader.ipynb",
+            op_reference(
+                "fn.readers.numpy",
+                "Loading Numpy array files (*.npy) to CPU or directly to GPU with GPUDirect Storage"
+            )),
     ])
--- docs/examples/index.py
+++ docs/examples/index.py
@@ -1,6 +1,10 @@
 doc(title="Examples and Tutorials",
     entries=[
-        "general/data_loading/index.py",
+        doc_entry(
+            "general/data_loading/index.py",
+            op_reference(
+                "fn.readers",
+                "Tutorial section describing how to read various data formats with DALI.")),
         "operations_index.py",
         "use_cases/index.py",
         "other_index.py",
