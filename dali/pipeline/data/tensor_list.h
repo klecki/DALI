8fa6d6ef565d35f51d6f41dfe1b0d0769b32c912
Krzysztof Lecki <klecki@nvidia.com>
2022-04-08 18:53:31 +0200

    WIP
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/pipeline/data/tensor_list.h
+++ dali/pipeline/data/tensor_list.h
@@ -826,6 +826,13 @@ class DLL_PUBLIC TensorList {
   }
 
   /** @} */  // end of ContiguousAccessorFunctions
+
+
+
+  // Next change removes the TensorList and replaces it by TensorVector. Heaving short lived
+  // access to the internals won't cause much coupling
+  template <typename InBackend>
+  friend class TensorVector;
 };
 
 }  // namespace dali
