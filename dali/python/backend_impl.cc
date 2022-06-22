af2ed1c88861116a793e94baf6d96deace2432d7
Krzysztof Lecki <klecki@nvidia.com>
2022-06-22 12:33:09 +0200

    Masacrating DALI by removing the TensorList
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>
    
    Almost works, the TV as TL
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>
    
    Masacrating changes
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>
    
    line breaks in commented commits
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>
    
    Fix to_decibels op
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>
    
    Tensor List tests
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/python/backend_impl.cc
+++ dali/python/backend_impl.cc
@@ -193,11 +193,11 @@ void FillTensorFromDlPack(py::capsule capsule, SourceDataType<SrcBackend> *batch
 
   // according to the docs kDLCUDAHost = kDLCPU | kDLCUDA so test it as a the first option
   if (dl_tensor.device.device_type == kDLCUDAHost) {
-    batch->set_device_id(-1);
+    // batch->set_device_id(-1);
   } else if (dl_tensor.device.device_type == kDLCPU) {
-    batch->set_device_id(-1);
+    // batch->set_device_id(-1);
   } else if (dl_tensor.device.device_type == kDLCUDA) {
-    batch->set_device_id(dl_tensor.device.device_id);
+    // batch->set_device_id(dl_tensor.device.device_id);
   } else {
     DALI_FAIL(make_string("Not supported DLPack device type: ", dl_tensor.device.device_type, "."));
   }
@@ -253,7 +253,7 @@ void FillTensorFromCudaArray(const py::object object, TensorType *batch, int dev
   if (device_id < 0) {
     CUDA_CALL(cudaGetDevice(&device_id));
   }
-  batch->set_device_id(device_id);
+  // batch->set_device_id(device_id);
 }
 
 void ExposeTensorLayout(py::module &m) {
@@ -795,7 +795,8 @@ void ExposeTensorList(py::module &m) {
           auto ret = std::make_shared<TensorList<GPUBackend>>();
           int dev = -1;
           CUDA_CALL(cudaGetDevice(&dev));
-          ret->set_device_id(dev);
+          // TODO ORDER!!!!
+          // ret->set_device_id(dev);
           UserStream *us = UserStream::Get();
           cudaStream_t s = us->GetStream(*ret);
           ret->Copy(t, s);
@@ -865,7 +866,7 @@ void ExposeTensorList(py::module &m) {
           std::string format;
           size_t type_size;
 
-          if (tl._num_elements() > 0) {
+          if (tl.shape().num_elements() > 0) {
             DALI_ENFORCE(IsValidType(tl.type()), "Cannot produce "
                 "buffer info for tensor w/ invalid type.");
             DALI_ENFORCE(tl.IsDenseTensor(),
@@ -933,7 +934,7 @@ void ExposeTensorList(py::module &m) {
 
       )code")
     .def("as_reshaped_tensor",
-        [](TensorList<CPUBackend> &tl, const vector<Index> &new_shape) -> Tensor<CPUBackend>* {
+        [](TensorList<CPUBackend> &tl, const vector<Index> &new_shape) {
           return tl.AsReshapedTensor(new_shape);
         },
       R"code(
@@ -941,15 +942,13 @@ void ExposeTensorList(py::module &m) {
 
       This function can only be called if `TensorList` is contiguous in memory and
       the volumes of requested `Tensor` and `TensorList` matches.
-      )code",
-      py::return_value_policy::reference_internal)
+      )code")
     .def("as_tensor", &TensorList<CPUBackend>::AsTensor,
       R"code(
       Returns a tensor that is a view of this `TensorList`.
 
       This function can only be called if `is_dense_tensor` returns `True`.
-      )code",
-      py::return_value_policy::reference_internal)
+      )code")
     .def("data_ptr",
         [](TensorList<CPUBackend> &tl) {
           return py::reinterpret_borrow<py::object>(
@@ -1039,6 +1038,7 @@ void ExposeTensorList(py::module &m) {
     .def("as_cpu", [](TensorList<GPUBackend> &t) {
           auto ret = std::make_shared<TensorList<CPUBackend>>();
           ret->set_pinned(false);
+          ret->SetContiguous(BatchState::Contiguous);
           UserStream * us = UserStream::Get();
           cudaStream_t s = us->GetStream(t);
           DeviceGuard g(t.device_id());
@@ -1124,7 +1124,7 @@ void ExposeTensorList(py::module &m) {
       return t.GetLayout().str();
     })
     .def("as_reshaped_tensor",
-        [](TensorList<GPUBackend> &tl, const vector<Index> &new_shape) -> Tensor<GPUBackend>* {
+        [](TensorList<GPUBackend> &tl, const vector<Index> &new_shape) {
           return tl.AsReshapedTensor(new_shape);
         },
       R"code(
@@ -1132,15 +1132,13 @@ void ExposeTensorList(py::module &m) {
 
       This function can only be called if `TensorList` is contiguous in memory and
       the volumes of requested `Tensor` and `TensorList` matches.
-      )code",
-      py::return_value_policy::reference_internal)
+      )code")
     .def("as_tensor", &TensorList<GPUBackend>::AsTensor,
       R"code(
       Returns a tensor that is a view of this `TensorList`.
 
       This function can only be called if `is_dense_tensor` returns `True`.
-      )code",
-      py::return_value_policy::reference_internal)
+      )code")
     .def("data_ptr",
         [](TensorList<GPUBackend> &tl) {
           return py::reinterpret_borrow<py::object>(
