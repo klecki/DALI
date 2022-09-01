17d1ee132ac9bc8c8aa31fafb1bb432958aabbdc
Krzysztof Lecki <klecki@nvidia.com>
2022-09-01 12:34:31 +0200

    To revert - needs the proper solution of setting order on inputs
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/pipeline/operator/builtin/split.cc
+++ dali/pipeline/operator/builtin/split.cc
@@ -66,9 +66,11 @@ void Split<Backend>::RunImpl(workspace_t<Backend> &ws) {
 
     // We can (and need to) do it only once, for each new output instance, when it doesn't have
     // data yet. It should be consistent across iterations.
+    // Maybe output.Reset()? We don't have any metadata.
     if (!output.has_data()) {
       output.SetupLike(input);
     }
+    // output.set_order() // set the input order without sync always
     output.SetSize(category_counts_[output_category]);
   }
 
@@ -82,6 +84,12 @@ void Split<Backend>::RunImpl(workspace_t<Backend> &ws) {
 
     // share the sample to the output
     output.UnsafeSetSample(output_idx, input, input_idx);
+  }
+  // Force the
+  auto stage_order = ws.has_stream() ? AccessOrder(ws.stream()) : AccessOrder::host();
+  for (int output_category = 0; output_category < kMaxCategories; output_category++) {
+    auto &output = ws.template Output<Backend>(output_category);
+    output.set_order(stage_order);
   }
 }
 
