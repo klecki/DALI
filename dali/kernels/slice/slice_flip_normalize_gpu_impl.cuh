22680a8e1e05b622584bb67bd46fff75055cb2cd
Krzysztof Lecki <klecki@nvidia.com>
2023-05-31 19:18:04 +0200

    Experiments
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/kernels/slice/slice_flip_normalize_gpu_impl.cuh
+++ dali/kernels/slice/slice_flip_normalize_gpu_impl.cuh
@@ -80,25 +80,74 @@ __global__ void SliceNormalizeKernel_2D(const SampleDesc<Out, In, 2> *samples,
 template <int static_channels, typename Out, typename In>
 __device__ void SliceNormalizeKernel_2D_NoPad_Ch(const SampleDesc<Out, In, 2> &sample,
                                               const ::dali::kernels::BlockDesc<2> &tile) {
-  auto fill_values = static_cast<const Out *>(sample.fill_values);
-  for (int y = threadIdx.y + tile.start.y; y < tile.end.y; y += blockDim.y) {
-    for (int x = threadIdx.x + tile.start.x; x < tile.end.x; x += blockDim.x) {
-      if constexpr (static_channels > 0) {
-        #pragma unroll static_channels
-        for (int c = 0; c < static_channels; c++) {
-          float fpin = sample.in(x, y, c);
-          float fpout = fmaf(fpin, sample.norm_mul[c], sample.norm_add[c]);
-          sample.out(x, y, c) = ConvertSat<Out>(fpout);
-        }
-      } else {
-        for (int c = 0; c < sample.in.channels; c++) {
-          float fpin = sample.in(x, y, c);
-          float fpout = fmaf(fpin, sample.norm_mul[c], sample.norm_add[c]);
-          sample.out(x, y, c) = ConvertSat<Out>(fpout);
-        }
+  constexpr int kTileSizeX = 64;
+  constexpr int kTileSizeY = 8;
+  __shared__ In input_tile[kTileSizeX * kTileSizeY * static_channels];
+  static_assert(static_channels > 0, "No support for dynamic channels in this variant");
+
+  __shared__ Out output_tiles[static_channels][kTileSizeX * kTileSizeY];
+  // if (threadIdx.x == 0 && threadIdx.y == 0 && blockIdx.x < 10) {
+  //   printf("%d-%d=%d %d-%d=%d; blockDim: %d, %d, %d\n", tile.start.y, tile.end.y, tile.end.y - tile.start.y, tile.start.x,
+  //          tile.end.x, tile.end.x - tile.start.x, blockDim.x, blockDim.y, blockDim.z);
+  // }
+
+  // sample.in
+
+  for (int y = threadIdx.y; y < tile.end.y - tile.start.y; y += blockDim.y) {
+    for (int x = threadIdx.x; x < (tile.end.x - tile.start.x) * static_channels; x += blockDim.x) {
+      int src_y = y + tile.start.y;
+      // assume stride in channels is 1 and stride in x is static_channels
+      int src_xc = x + tile.start.x * static_channels;
+      int64_t src = src_y * sample.in.strides.y + src_xc;
+      // TODO: OOB
+      input_tile[y * static_channels * kTileSizeX + x] = 42;//sample.in.data[src];
+    }
+  }
+
+  // TODO: bank conflicts
+
+  __syncthreads();
+
+  for (int y = threadIdx.y; y < tile.end.y - tile.start.y; y += blockDim.y) {
+    for (int x = threadIdx.x; x < (tile.end.x - tile.start.x); x += blockDim.x) {
+      #pragma unroll static_channels
+      for (int c = 0; c < static_channels; c++) {
+        float fpin = input_tile[y * static_channels * kTileSizeX + x * static_channels + c];
+        float fpout = fmaf(fpin, sample.norm_mul[c], sample.norm_add[c]);
+        output_tiles[c][y * kTileSizeX + x] = ConvertSat<Out>(fpout);
       }
     }
   }
+  __syncthreads();
+
+  for (int c = 0; c < static_channels; c++) {
+    for (int y = threadIdx.y; y < tile.end.y - tile.start.y; y += blockDim.y) {
+      for (int x = threadIdx.x; x < (tile.end.x - tile.start.x); x += blockDim.x) {
+        sample.out(x + tile.start.x, y + tile.start.y, c) = output_tiles[c][y * kTileSizeX + x];
+      }
+    }
+  }
+
+
+
+  // for (int y = threadIdx.y + tile.start.y; y < tile.end.y; y += blockDim.y) {
+  //   for (int x = threadIdx.x + tile.start.x; x < tile.end.x; x += blockDim.x) {
+  //     if constexpr (static_channels > 0) {
+  //       #pragma unroll static_channels
+  //       for (int c = 0; c < static_channels; c++) {
+  //         float fpin = sample.in(x, y, c);
+  //         float fpout = fmaf(fpin, sample.norm_mul[c], sample.norm_add[c]);
+  //         sample.out(x, y, c) = ConvertSat<Out>(fpout);
+  //       }
+  //     } else {
+  //       for (int c = 0; c < sample.in.channels; c++) {
+  //         float fpin = sample.in(x, y, c);
+  //         float fpout = fmaf(fpin, sample.norm_mul[c], sample.norm_add[c]);
+  //         sample.out(x, y, c) = ConvertSat<Out>(fpout);
+  //       }
+  //     }
+  //   }
+  // }
 }
 
 template <typename Out, typename In>
@@ -106,9 +155,12 @@ __global__ void SliceNormalizeKernel_2D_NoPad(const SampleDesc<Out, In, 2> *samp
                                               const ::dali::kernels::BlockDesc<2> *tiles) {
   const auto tile = tiles[blockIdx.x];
   const auto sample = samples[tile.sample_idx];
-  VALUE_SWITCH(sample.out.channels, static_channels, (1, 2, 3, 4, 5, 6, 7, 8, 16),
+  // TODO:  5, 6, 7, 8, 16
+  VALUE_SWITCH(sample.out.channels, static_channels, (1, 2, 3, 4),
     (SliceNormalizeKernel_2D_NoPad_Ch<static_channels>(sample, tile);),
-    (SliceNormalizeKernel_2D_NoPad_Ch<-1>(sample, tile);)
+    (
+      //SliceNormalizeKernel_2D_NoPad_Ch<-1>(sample, tile);
+      )
   );  // NOLINT(whitespace/parens)
 }
 
